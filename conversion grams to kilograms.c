ba4940e9fe4d137c8499ea8bdcdf33ba7abbe80d
KoushikGajulapalli <120742225+KoushikGajulapalli@users.noreply.github.com>
2022-12-16 22:54:32 +0530

    Create conversion grams to kilograms.c

---
+++ conversion grams to kilograms.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+int void()
+{
+    float grams;
+    printf("please enter grams :");
+    scanf("%d",&grams);
+    
+    float kilograms = grams/1000;
+    
+    printf("%d kilograms",kilograms);
